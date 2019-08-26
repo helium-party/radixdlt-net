67337f24448123df78a9e27ef34d78246ee305f5
jonas452 <jonas.six452@gmail.com>
2019-08-26 17:50:55 +0200

    base implementation of radixidentities : todo DSON Hash

---
+++ HeliumParty.RadixDLT/src/HeliumParty.RadixDLT.App/Identity/BaseRadixIdentity.cs
@@ -0,0 +1,12 @@
+﻿using HeliumParty.RadixDLT.EllipticCurve.Managers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeliumParty.RadixDLT.App.Identity
+{
+    public class BaseRadixIdentity
+    {
+        public IECKeyManager KeyManager { get; set; }
+    }
+}
