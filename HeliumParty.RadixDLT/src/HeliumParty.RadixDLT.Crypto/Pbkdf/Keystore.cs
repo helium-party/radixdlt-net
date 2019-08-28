1b0c7812647db4fd944990703c91703c78ec83eb
jonas452 <jonas.six452@gmail.com>
2019-08-28 17:19:31 +0200

    Created a keystore. : is a way to store details on how to generate a password. aka salt,hash,iteration,...

---
+++ HeliumParty.RadixDLT/src/HeliumParty.RadixDLT.Crypto/Pbkdf/Keystore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeliumParty.RadixDLT.Pbkdf
+{
+    public class Keystore
+    {
+        public string Id { get; set; }
+        public CryptoDetails CryptoDetails { get; set; }
+
+    }
+
+    public class CryptoDetails
+    {
+        public string Cipher { get; set; }
+        public CipherParams CipherParams { get; set; }
+        public string CipherText { get; set; }
+        public Pbkdfparams Pbkdfparams { get; set; }
+        public string Mac { get; set; }
+
+    }
+}
---
+++ HeliumParty.RadixDLT/src/HeliumParty.RadixDLT.Crypto/Pbkdf/Pbkdfparams.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeliumParty.RadixDLT.Pbkdf
+{
+    public class Pbkdfparams
+    {
+        public int Iterations { get; set; }
+        public int KeyLenght { get; set; }
+        public string Digest { get; set; }
+        public string Salt { get; set; }
+    }
+}
