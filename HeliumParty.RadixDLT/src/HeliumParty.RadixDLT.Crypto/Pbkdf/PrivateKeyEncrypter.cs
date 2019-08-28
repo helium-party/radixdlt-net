c5c47b4ebaebccbf97a89da6c298c954d1116a37
jonas452 <jonas.six452@gmail.com>
2019-08-28 17:28:25 +0200

    started on keyencrypter

---
+++ HeliumParty.RadixDLT/src/HeliumParty.RadixDLT.Crypto/Pbkdf/PrivateKeyEncrypter.cs
@@ -0,0 +1,20 @@
+﻿using HeliumParty.RadixDLT.EllipticCurve;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeliumParty.RadixDLT.Pbkdf
+{
+    public static class PrivateKeyEncrypter
+    {
+        public static ECPrivateKey Decrypt(string pasword, Keystore keystore)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Keystore Encrypt(string pasword, ECPrivateKey privatekey)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
