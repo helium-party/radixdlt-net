a9ffb88c0edc0a1fb80164a0c1d8100bb59796b0
jonas452 <jonas.six452@gmail.com>
2019-08-28 17:09:09 +0200

    started on keygeneration. Issue with salt. at this time, salt is random.

---
+++ HeliumParty.RadixDLT/src/HeliumParty.RadixDLT.Crypto/CryptoGraphicKeyGenerator.cs
@@ -0,0 +1,24 @@
+﻿using HeliumParty.RadixDLT.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace HeliumParty.RadixDLT
+{
+    public static class CryptoGraphicKeyGenerator
+    {
+        public static byte[] Generate(string password)
+        {
+            byte[] salt;
+            byte[] key;
+            using (var derivedBytes = new Rfc2898DeriveBytes(password, 16, iterations: 50000))
+            {
+                salt = derivedBytes.Salt;
+                key = derivedBytes.GetBytes(16);               
+            }
+
+            return Arrays.ConcatArrays(key, salt);
+        }
+    }
+}
