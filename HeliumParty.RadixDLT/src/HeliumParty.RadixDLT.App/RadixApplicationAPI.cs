1c1d51a89769912a5cfeceed179a5f24f188595e
jonas452 <jonas.six452@gmail.com>
2019-08-30 17:03:49 +0200

    started on api builder

---
+++ HeliumParty.RadixDLT/src/HeliumParty.RadixDLT.App/RadixApplicationAPI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeliumParty.RadixDLT
+{
+    public class RadixApplicationAPI
+    {
+        public static RadixApplicationAPI Create()
+        {
+            throw new NotImplementedException("todo"); 
+        }
+    }
+
+}
---
+++ HeliumParty.RadixDLT/src/HeliumParty.RadixDLT.App/RadixApplicationAPIBuilder.cs
@@ -0,0 +1,32 @@
+﻿using HeliumParty.RadixDLT.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeliumParty.RadixDLT
+{
+    public class RadixApplicationAPIBuilder
+    {
+        private IRadixIdentity _identity;
+        private object _universe;//needs networklayer
+
+        public RadixApplicationAPIBuilder Identity(IRadixIdentity identity)
+        {
+            _identity = identity;
+            return this;
+        }
+
+        public RadixApplicationAPIBuilder Universe(object universe)
+        {
+            _universe = universe;
+            return this;
+        }
+
+        public static RadixApplicationAPIBuilder Builder()
+        {
+            return new RadixApplicationAPIBuilder();
+        }
+    }
+
+    
+}
