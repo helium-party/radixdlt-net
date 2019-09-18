76ca93e5a3bac818e1c0d1cef3d6ba8deacc3a5d
Christian <christian.ganhoer@outlook.com>
2019-09-18 12:29:23 +0200

    'Dumb' logger implementation for this branch.

---
+++ HeliumParty.RadixDLT/src/HeliumParty.RadixDLT.Utils/Log/ILogger.cs
@@ -0,0 +1,19 @@
+﻿namespace HeliumParty.RadixDLT.Log
+{
+    public interface ILogger
+    {
+        bool IsDebugEnabled { get; set; }
+
+        /// <summary>
+        /// Defines the minimum level of messages to log
+        /// </summary>
+        LogLevel LogLevel { get; set; }
+
+        /// <summary>
+        /// Logs the specified message
+        /// </summary>
+        /// <param name="message">The message</param>
+        /// <param name="level">The log level of the message</param>
+        void LogMessage(string message, LogLevel level = LogLevel.Verbose);
+    }
+}
---
+++ HeliumParty.RadixDLT/src/HeliumParty.RadixDLT.Utils/Log/LogLevel.cs
@@ -0,0 +1,20 @@
+﻿namespace HeliumParty.RadixDLT.Log
+{
+    public enum LogLevel
+    {
+        // Additional information
+        Verbose = 0,
+
+        // Useful for debugging
+        Debug = 1,
+
+        // Critical log information
+        Critical = 2,
+        
+        // Don't log any messages
+        None = 4,
+
+        // General information
+        Information = 99
+    }
+}
