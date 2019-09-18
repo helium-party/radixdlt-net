5e89b8f4f6092abcbcb58a8bb6d1b3d9cb292af1
Christian <christian.ganhoer@outlook.com>
2019-09-18 12:29:23 +0200

    'Dumb' logger implementation for this branch.

---
+++ HeliumParty.RadixDLT/src/HeliumParty.RadixDLT.Utils/Log/OutputLogger.cs
@@ -0,0 +1,34 @@
+﻿namespace HeliumParty.RadixDLT.Log
+{
+    public class OutputLogger : ILogger
+    {
+        public LogLevel LogLevel { get; set; }
+        public bool IsDebugEnabled { get; set; }
+
+        public const int MaxMessageSize = 1024;
+
+        public void LogMessage(string message, LogLevel level = LogLevel.Verbose)
+        {
+            System.Diagnostics.Debug.Print($"{BuildTimeStamp()} - {message}");
+        }
+
+        private string BuildTimeStamp()
+        {
+            var dtn = System.DateTime.Now;
+            return $"{ dtn.ToShortDateString()} - { dtn.ToLongTimeString()} {dtn.Millisecond.ToString("D3")}";
+        }
+
+        /// <summary>
+        /// Ensures that the message doesn't exceed the specified max length
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        private string EnsureMessageLength(string message)
+        {
+            if (message.Length > MaxMessageSize)
+                return message.Substring(0, MaxMessageSize - 3 /*for the '...'*/) + "...";
+            else
+                return message;
+        }
+    }
+}
