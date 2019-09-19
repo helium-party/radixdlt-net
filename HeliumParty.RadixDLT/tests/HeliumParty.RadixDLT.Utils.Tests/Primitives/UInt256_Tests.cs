0b0b6862f53ce0dd856aad234ca3d6c59df52b32
jonas452 <jonas.six452@gmail.com>
2019-09-19 20:09:44 +0200

    implementation of Uint256

---
+++ HeliumParty.RadixDLT/tests/HeliumParty.RadixDLT.Utils.Tests/Primitives/UInt256_Tests.cs
@@ -0,0 +1,26 @@
+﻿using HeliumParty.RadixDLT.Primitives;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace HeliumParty.RadixDLT.Utils.Tests.Primitives
+{
+    public class UInt256_Tests
+    {
+        [Fact]
+        public void IntToByteConversionTest()
+        {
+            //arrange
+            var bytes = RandomGenerator.GetRandomBytes(32);
+
+            //act
+            UInt256 bigint = bytes;
+            byte[] bytesnew = bigint;
+
+            //assert
+            bytes.ShouldBe(bytesnew);
+        }
+    }
+}
