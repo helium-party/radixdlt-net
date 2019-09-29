7d49029fa85147917ba924bcc1848e4f8e12d32a
jonassix <jonas.six452@gmail.com>
2019-09-29 16:31:14 +0200

    create 2 attributes to set and expose dependency that differes from convention

---
+++ HeliumParty.RadixDLT/src/HeliumParty.DependencyInjection/DependencyAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeliumParty.DependencyInjection
+{
+    public class DependencyAttribute : Attribute
+    {
+        public virtual bool TryRegister { get; set; }
+
+        public virtual bool ReplaceServices { get; set; }
+
+        public DependencyAttribute()
+        {
+
+        }
+    }
+}
