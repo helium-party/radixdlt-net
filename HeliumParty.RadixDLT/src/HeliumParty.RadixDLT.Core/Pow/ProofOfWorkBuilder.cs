ba11e98da2dcc2c2b449c92a6a377a20cf8cf393
jonassix <jonas.six452@gmail.com>
2019-10-13 17:54:35 +0200

    Bug fix - pow builder

--- HeliumParty.RadixDLT/src/HeliumParty.RadixDLT.Core/Pow/ProofOfWorkBuilder.cs
+++ HeliumParty.RadixDLT/src/HeliumParty.RadixDLT.Core/Pow/ProofOfWorkBuilder.cs
@@ -12,48 +12,58 @@ namespace HeliumParty.RadixDLT.Pow
 {
     public class ProofOfWorkBuilder
     {
-        public Task<ProofOfWork> Build(int magic, byte[] seed, int leading)
+        public Task<ProofOfWork> BuildAsync(int magic, byte[] seed, int leading)
         {
             return new Task<ProofOfWork>(() => 
             {
-                if (seed.Length != 32 || leading < 1 || leading > 256)
-                    throw new ArgumentException();
-
-                BitArray targetBitSet = new BitArray(256);
-
-                targetBitSet.SetAll(true);
-
-                for (int i = 0; i < leading; i++)
-                    targetBitSet.Set(i, false);
-
-                for (int i = leading + (8 - leading % 8); i < leading + 8; i++)
-                    targetBitSet.Set(i, false);
-
-                byte[] target = new byte[targetBitSet.Length / 8];
-                targetBitSet.CopyTo(target, 0);
-
-                using (var stream = new MemoryStream(32 + 4 + 8))
-                {
-                    var nonce = 1L;
-                    var magicbytes = BitConverter.GetBytes(magic);
-                    var targetHex = Bytes.ToHexString(target);
-
-                    stream.Write(magicbytes, 0, magicbytes.Length);
-                    stream.Write(seed, 0, seed.Length);
-
-                    while (true)
-                    {
-                        stream.Position = 32 + 4;
-                        var nonceBytes = BitConverter.GetBytes(nonce);
-                        stream.Write(nonceBytes, 0, nonceBytes.Length);
-                        var hashHex = Bytes.ToHexString(RadixHash.From(stream.ToArray()).ToByteArray());
-                        if (hashHex.CompareTo(targetHex) < 0)
-                            return new ProofOfWork(nonce, magic, seed, target);
-
-                        nonce++;
-                    }
-                }
+                return Build(magic,seed,leading);
             });           
         }
+
+        public ProofOfWork Build(int magic, byte[] seed, int leading)
+        {
+            if (seed.Length != 32 || leading < 1 || leading > 256)
+                throw new ArgumentException();
+
+            BitArray targetBitSet = new BitArray(256);
+
+            targetBitSet.SetAll(true);
+
+            int lead = leading / 8;
+            lead = lead * 8;//losing precision
+
+            for (int i = 0; i < lead; i++)
+                targetBitSet.Set(i, false);
+
+            for (int i = lead + (8 - leading % 8); i < lead + 8; i++)
+                targetBitSet.Set(i, false);
+
+            byte[] target = new byte[targetBitSet.Length / 8];
+            targetBitSet.CopyTo(target, 0);
+
+            using (var stream = new MemoryStream(32 + 4 + 8))
+            {
+                var nonce = 1L;
+                var magicbytes = BitConverter.GetBytes(magic);
+                var targetHex = Bytes.ToHexString(target);
+
+                stream.Write(magicbytes, 0, magicbytes.Length);
+                stream.Write(seed, 0, seed.Length);
+
+                while (true)
+                {
+                    stream.Position = 32 + 4;
+                    var nonceBytes = BitConverter.GetBytes(nonce);
+                    stream.Write(nonceBytes, 0, nonceBytes.Length);
+                    var hashHex = Bytes.ToHexString(RadixHash.From(stream.ToArray()).ToByteArray());
+                    if (hashHex.CompareTo(targetHex) < 0)
+                        return new ProofOfWork(nonce, magic, seed, target);
+
+                    nonce++;
+                }
+            }
+        }
     }
+
+   
 }
