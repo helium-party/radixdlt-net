2392f6e48316045239b81cccb555c1067e2d15c9
jonassix <jonas.six452@gmail.com>
2019-10-23 20:57:02 +0200

    Decrypted message

---
+++ HeliumParty.RadixDLT/src/HeliumParty.RadixDLT.App/Messages/DecryptedMessage.cs
@@ -0,0 +1,32 @@
+﻿using HeliumParty.RadixDLT.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeliumParty.RadixDLT.Messages
+{
+    public class DecryptedMessage
+    {
+        public RadixAddress From { get; }
+        public RadixAddress To { get; }
+        public byte[] Data { get; }
+        public MessageEncryptionState EncryptionState { get; }
+        public long TimeStamp { get;  }
+        public EUID ActionId { get;  }
+
+        public DecryptedMessage(RadixAddress from, RadixAddress to, byte[] data, MessageEncryptionState encryptionState, long timeStamp, EUID actionId)
+        {
+            From = from;
+            To = to;
+            Data = data;
+            EncryptionState = encryptionState;
+            TimeStamp = timeStamp;
+            ActionId = actionId;
+        }
+
+        public override string ToString()
+        {
+            return $"timestamp {From} -> {To}: {EncryptionState} {Convert.ToBase64String(Data)}";
+        }
+    }
+}
---
+++ HeliumParty.RadixDLT/src/HeliumParty.RadixDLT.App/Messages/MessageEncryptionState.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeliumParty.RadixDLT.Messages
+{
+    public enum MessageEncryptionState
+    {
+        /**
+         * Specifies that the data in the DecryptedMessage object WAS originally
+         * encrypted and has been successfully decrypted to it's present byte array.
+         */
+        DECRYPTED,
+        /**
+		 * Specifies that the data in the DecryptedMessage object was NOT
+		 * encrypted and the present data byte array just represents the original data.
+		 */
+        NOT_ENCRYPTED,
+        /**
+		 * Specifies that the data in the DecryptedMessage object WAS encrypted
+		 * but could not be decrypted. The present data byte array represents the still
+		 * encrypted data.
+		 */
+        CANNOT_DECRYPT,
+    }
+}
