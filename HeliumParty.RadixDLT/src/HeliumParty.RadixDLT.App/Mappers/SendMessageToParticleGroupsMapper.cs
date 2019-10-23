f7cf5c4caaaad144955ae58eb30d2a113cf90306
jonassix <jonas.six452@gmail.com>
2019-10-23 21:14:03 +0200

    Working on message to particlegroup action

---
+++ HeliumParty.RadixDLT/src/HeliumParty.RadixDLT.App/Mappers/SendMessageToParticleGroupsMapper.cs
@@ -0,0 +1,20 @@
+﻿using HeliumParty.RadixDLT.Actions;
+using HeliumParty.RadixDLT.Atoms;
+using HeliumParty.RadixDLT.EllipticCurve;
+using HeliumParty.RadixDLT.EllipticCurve.Managers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeliumParty.RadixDLT.Mappers
+{
+    public class SendMessageToParticleGroupsMapper : IStatelessActionToParticleGroupsMapper<SendMessageAction>
+    {
+        private readonly IECKeyManager _keyManager;
+        private readonly Func<SendMessageAction, IEnumerable<ECPublicKey>> _encryptionScheme;
+        public List<ParticleGroup> MapToParticleGroup(SendMessageAction action)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
