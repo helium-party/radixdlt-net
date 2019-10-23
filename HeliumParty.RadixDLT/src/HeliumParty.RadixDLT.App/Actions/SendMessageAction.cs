3674916b5342163f96ea65035d84a485a16319f2
jonassix <jonas.six452@gmail.com>
2019-10-23 20:41:50 +0200

    Created message action

---
+++ HeliumParty.RadixDLT/src/HeliumParty.RadixDLT.App/Actions/SendMessageAction.cs
@@ -0,0 +1,28 @@
+﻿using HeliumParty.RadixDLT.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeliumParty.RadixDLT.Actions
+{
+    public class SendMessageAction : IAction
+    {
+        public RadixAddress From { get; }
+        public RadixAddress To { get; }
+        public byte[] Data { get; }
+        public bool Encrypt { get;  }
+
+        public SendMessageAction(RadixAddress from, RadixAddress to, byte[] data, bool encrypt)
+        {
+            From = from;
+            To = to;
+            Data = data;
+            Encrypt = encrypt;
+        }
+
+        public override string ToString()
+        {
+            return $"SEND MESSAGE FROM {From} TO {To}";
+        }
+    }
+}
