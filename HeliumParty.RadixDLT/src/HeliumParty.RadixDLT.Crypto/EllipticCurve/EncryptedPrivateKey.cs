533b1cd2a52f4fd5323ed38001aa0712de59f029
jonassix <jonas.six452@gmail.com>
2019-10-26 16:42:11 +0200

    object for holding a encrypted private key

---
+++ HeliumParty.RadixDLT/src/HeliumParty.RadixDLT.Crypto/EllipticCurve/EncryptedPrivateKey.cs
@@ -0,0 +1,26 @@
+﻿using HeliumParty.RadixDLT.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeliumParty.RadixDLT.EllipticCurve
+{
+    public class EncryptedPrivateKey : IBase64Encoded
+    {
+        private readonly byte[] _encryptedKey;
+
+        public EncryptedPrivateKey(byte[] encryptedKey)
+        {
+            _encryptedKey = encryptedKey;
+        }
+
+        public EncryptedPrivateKey(string base64Encryptedkey)
+            : this (Convert.FromBase64String(base64Encryptedkey))
+        {
+        }
+
+        public string Base64 => Convert.ToBase64String(_encryptedKey);
+
+        public byte[] Base64Array => Arrays.CopyOfRange(_encryptedKey, 0, _encryptedKey.Length);
+    }
+}
