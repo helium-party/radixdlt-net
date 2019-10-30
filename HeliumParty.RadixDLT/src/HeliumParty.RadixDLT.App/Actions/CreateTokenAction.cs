d0d077ad2bcf60c705eb4bc856d3991bace70e08
jonassix <jonas.six452@gmail.com>
2019-10-30 11:19:44 +0100

    CreateAction

---
+++ HeliumParty.RadixDLT/src/HeliumParty.RadixDLT.App/Actions/CreateTokenAction.cs
@@ -0,0 +1,46 @@
+﻿using HeliumParty.RadixDLT.Identity;
+using HeliumParty.RadixDLT.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeliumParty.RadixDLT.Actions
+{
+    public enum TokenSupplyType
+    {
+        Fixed,
+        Mutable
+    }
+
+
+    public class CreateTokenAction : IAction
+    {
+        public string Name { get; protected set; }
+        public RRI RRI { get; protected set; }
+        public string IconUrl { get; set; }
+        public BigDecimal InitialSupply { get; protected set; }
+        public BigDecimal Granularity { get; protected set; }
+        public TokenSupplyType TokenSupplyType { get; protected set; }
+
+        public CreateTokenAction(string name, RRI rRI, string iconUrl, BigDecimal initialSupply, BigDecimal granularity, TokenSupplyType tokenSupplyType)
+        {
+            if (initialSupply < 0)
+                throw new ArgumentException(nameof(initialSupply) + " cannot be less then 0");
+
+            if(tokenSupplyType == TokenSupplyType.Fixed && initialSupply == 0)
+                throw new ArgumentException(nameof(initialSupply) + " Must be greater then 0");
+
+            Name = name;
+            RRI = rRI;
+            IconUrl = iconUrl;
+            InitialSupply = initialSupply;
+            Granularity = granularity;
+            TokenSupplyType = tokenSupplyType;
+        }
+
+        public override string ToString()
+        {
+            return $"Create Token {RRI} {TokenSupplyType}";
+        }
+    }
+}
