272aec8a5a353a044687c36ee2987b647b6f6a3d
jonassix <jonas.six452@gmail.com>
2019-10-30 15:09:29 +0100

    started on createTokentoParticlegroupsmapper

---
+++ HeliumParty.RadixDLT/src/HeliumParty.RadixDLT.App/Mappers/CreateTokenToParticleGroupsMapper.cs
@@ -0,0 +1,27 @@
+﻿using HeliumParty.RadixDLT.Actions;
+using HeliumParty.RadixDLT.Atoms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeliumParty.RadixDLT.Mappers
+{
+    public class CreateTokenToParticleGroupsMapper : IStatelessActionToParticleGroupsMapper<CreateTokenAction>
+    {
+        public List<ParticleGroup> MapToParticleGroups(CreateTokenAction action)
+        {
+            if (action.TokenSupplyType == TokenSupplyType.Fixed)
+            {
+                return createFixedSupplyToken(tokenCreation);
+            }
+            else if (action.TokenSupplyType == TokenSupplyType.Mutable)
+            {
+                return createVariableSupplyToken(action);
+            }
+            else
+            {
+                throw new ArgumentException("Unknown supply type: " + action.TokenSupplyType);
+            }
+        }
+    }
+}
