91277f59667d91c8a1490c52ab9e35b994417858
jonassix <jonas.six452@gmail.com>
2019-11-07 17:56:21 +0100

    todo token conversion

---
+++ HeliumParty.RadixDLT/src/HeliumParty.RadixDLT.App/Actions/TokenUnitConversions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HeliumParty.RadixDLT.Primitives;
+
+namespace HeliumParty.RadixDLT.Actions
+{
+    public static class TokenUnitConversions
+    {
+        public static UInt256 UnitsToSubUnits(BigDecimal initialSupply)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
