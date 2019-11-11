51258358ccfc74c4606a1229afbff96229410386
jonassix <jonas.six452@gmail.com>
2019-11-11 20:17:00 +0100

    impl fixedtokenmapper

--- HeliumParty.RadixDLT/src/HeliumParty.RadixDLT.App/Mappers/CreateTokenToParticleGroupsMapper.cs
+++ HeliumParty.RadixDLT/src/HeliumParty.RadixDLT.App/Mappers/CreateTokenToParticleGroupsMapper.cs
@@ -1,5 +1,7 @@
 ﻿using HeliumParty.RadixDLT.Actions;
 using HeliumParty.RadixDLT.Atoms;
+using HeliumParty.RadixDLT.Identity.Managers;
+using HeliumParty.RadixDLT.Particles;
 using HeliumParty.RadixDLT.Particles.Types;
 using HeliumParty.RadixDLT.Primitives;
 using System;
@@ -10,6 +12,13 @@ namespace HeliumParty.RadixDLT.Mappers
 {
     public class CreateTokenToParticleGroupsMapper : IStatelessActionToParticleGroupsMapper<CreateTokenAction>
     {
+        private readonly IEUIDManager _euidManager;
+
+        public CreateTokenToParticleGroupsMapper(IEUIDManager euidManager)
+        {
+            _euidManager = euidManager;
+        }
+
         public List<ParticleGroup> MapToParticleGroups(CreateTokenAction action)
         {
             if (action.TokenSupplyType == TokenSupplyType.Fixed)
@@ -45,8 +54,28 @@ namespace HeliumParty.RadixDLT.Mappers
                 granularity,
                 action.IconUrl);
 
+
+            var tokens = new TransferableTokensParticle(
+                token.RRI.Address,
+                token.RRI,
+                granularity,
+                DateTimeOffset.Now.ToUnixTimeMilliseconds() / 60000L + 60000L,
+                DateTimeOffset.Now.ToUnixTimeMilliseconds(),
+                amount,
+                new Dictionary<TokenTransition, TokenPermission>(),
+                _euidManager.GetEUID(token.Address));
+
+            var rriParticle = new RRIParticle(token.RRI, _euidManager.GetEUID(token.RRI.Address));
+
             
-            throw new NotImplementedException();
+            var tokenCreationGroup = new ParticleGroup(new List<SpunParticle>() 
+            { 
+                SpunParticle.Down(rriParticle),
+                SpunParticle.Up(token),
+                SpunParticle.Up(tokens)
+            });
+
+            return new List<ParticleGroup>() { tokenCreationGroup };
         }
     }
 }
