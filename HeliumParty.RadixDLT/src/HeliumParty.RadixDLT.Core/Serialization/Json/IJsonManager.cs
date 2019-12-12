82338a10c63c91d38e13ad820292aedb2db01d8f
TMaddox <github@mailexpress.cc>
2019-12-12 17:03:49 +0100

    implemented JsonManager and required classes

---
+++ HeliumParty.RadixDLT/src/HeliumParty.RadixDLT.Core/Serialization/Json/IJsonManager.cs
@@ -0,0 +1,9 @@
+﻿namespace HeliumParty.RadixDLT.Serialization.Json
+{
+    public interface IJsonManager
+    {
+        T FromJson<T>(string str, OutputMode mode = OutputMode.All);
+
+        string ToJson<T>(T obj, OutputMode mode = OutputMode.All);
+    }
+}
